8b8da2d74acb20dc9fa72a64a035a3af373aed17
JDAS <joseangarita1126@gmail.com>
2025-12-09 10:08:23 -0500

    a

--- database/migrations/2025_09_25_172613_create_vehiculos_table.php
+++ database/migrations/2025_09_25_172613_create_vehiculos_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->foreignId('tipos_vehiculos_id')->constrained();
             $table->string('estado');
             $table->string('registradoPor');
-            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
